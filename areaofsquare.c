45beaa2d883bcc39212a10d1f691dfa11713db0f
Jahanzab-Jhandeer <74298308+Jahanzab-Jhandeer@users.noreply.github.com>
2022-07-30 15:46:26 +0500

    Add files via upload

---
+++ areaofsquare.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+// find area of square
+int main () {
+    int side;
+    printf("type the area of one side :");
+    scanf("%d" , &side);
+    printf("%d\n" , side * side);
+    return 0;
+}
